2cdd65a53c9d5aa55184a4ba47cf3d627dfeb336
Paulo Henrique Silva <yogscastfriend@gmail.com>
2026-05-08 15:55:43 -0300

    Update prova_imc.py

--- prova_imc.py
+++ prova_imc.py
@@ -10,8 +10,8 @@ print ( )
 #^ informações basicas
 peso = float(input("Digite o seu peso: "))
 altura = float(input("Digite a sua altura: "))
-print (f"O seu peso é", peso)
-print (f"A sua altura é", altura)
+print (f"O seu peso é", peso, "kg")
+print (f"A sua altura é", altura, "m")
 IMC = peso / (altura ** 2) #conta
 print(f"IMC: {IMC:.2f}") #print com formatação
 if IMC >= 30.0:
