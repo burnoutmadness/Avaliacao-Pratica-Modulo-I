0c171d6d865bedf5016308ffb9f5495fb2b3a949
Paulo Henrique Silva <yogscastfriend@gmail.com>
2026-05-08 15:31:03 -0300

    Create prova_imc.py

---
+++ prova_imc.py
@@ -0,0 +1,20 @@
+#programa para calcular IMC
+nome = input ("Digite o seu nome: ")
+print ( )
+print ("Nome registrado:", nome)
+print ( )
+idade = input ("Digite a sua idade: ")
+print ( )
+print ("Idade registrada:", idade)
+print ( )
+#^ informações basicas
+peso = float(input("Digite o seu peso: "))
+altura = float(input("Digite a sua altura: "))
+print (f"O seu peso é", peso)
+print (f"A sua altura é", altura)
+IMC = peso / (altura ** 2) #conta
+print(f"IMC: {IMC:.2f}") #print com formatação
+if IMC >= 30.0:
+    print("Cuidado com a sua saúde.")
+elif IMC <= 30.0:
+    print ("A sua saúde esta boa.")
